8f469f6a9c683793918cf211be3240238fc4565a
Henrique Montenegro <montenegrohenrique@MonteBook.local>
2019-01-08 23:27:05 -0200

    Aula 02.7

---
+++ client/js/app/models/Negociacao.js
@@ -0,0 +1,27 @@
+class Negociacao {
+
+    constructor(data, quantidade, valor){
+
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+
+        Object.freeze(this); // congela o objeto não permitindo que ele seja alterado apóes sua criação
+    }
+
+    get volume(){
+        return this._quantidade * this._valor;
+    }
+
+    get data() {
+        return this._data;
+    }
+
+    get quantidade() {
+        return this._quantidade;
+    }
+
+    get valor() {
+        return this._valor;
+    }
+}
