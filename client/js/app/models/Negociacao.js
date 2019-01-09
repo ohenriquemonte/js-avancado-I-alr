2c3302d4aed351ac7cb481a8f31bec9dd81632c1
Henrique Montenegro <montenegrohenrique@MonteBook.local>
2019-01-08 23:33:45 -0200

    Aula 02.8 - Programação defensiva

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -2,7 +2,7 @@ class Negociacao {
 
     constructor(data, quantidade, valor){
 
-        this._data = data;
+        this._data =  new Date(data);
         this._quantidade = quantidade;
         this._valor = valor;
 
@@ -14,7 +14,7 @@ class Negociacao {
     }
 
     get data() {
-        return this._data;
+        return new Date(this._data.getTime());
     }
 
     get quantidade() {
