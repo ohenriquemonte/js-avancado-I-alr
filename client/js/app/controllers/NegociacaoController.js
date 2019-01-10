aae537a7124cc89bdf829a42c7c027b7646bd5d4
Henrique Montenegro <montenegrohenrique@MonteBook.local>
2019-01-09 23:03:25 -0200

    Aula 03.8 - Resolvendo um problema com o paradigma funcional

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -11,7 +11,19 @@ class NegociacaoController {
         event.preventDefault();
 
         // let data = new Date (this._inputData.value.split('-'));
-        let data = new Date (this._inputData.value.replace(/-/g, ','));
+        // let data = new Date (this._inputData.value.replace(/-/g, ','));
+        let data = new Date (...
+            this._inputData.value
+                .split('-')
+                .map(function(item, indice){
+                    // if(indice == 1) {
+                    //     return item - 1;
+                    // }
+                    // return item;
+
+                    return item - indice % 2; // decrementa 1 na posicao 1 (mes)
+                })
+        ); // utilizando spread operator
     
 
         // adicionar a negociacao em uma lista
