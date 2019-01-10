e0f24364c630f28b280a6297b04d790d811e3376
Henrique Montenegro <montenegrohenrique@MonteBook.local>
2019-01-09 22:04:51 -0200

    Aula 03.2 - Associando métodos do controller às ações do usuário

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -2,7 +2,16 @@ class NegociacaoController {
 
     adiciona(event) {
         event.preventDefault();
-        alert('chamei a acao do controller');
+        
+        let $ = document.querySelector.bind(document); // bind mantém a associação com document
+
+        let inputData = $('#data');
+        let inputValor = $('#valor');
+        let inputQuantidade = $('#quantidade');
+
+        console.log(inputData.value);
+        console.log(inputQuantidade.value);
+        console.log(inputValor.value);
     }
 
 }
