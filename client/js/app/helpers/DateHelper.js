99653852dbcc1120a0c828a4bd446da0bb654419
Henrique Montenegro <montenegrohenrique@MonteBook.local>
2019-01-13 13:07:24 -0200

    Aula 04.4 - Template Strings

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -5,12 +5,18 @@ class DateHelper {
     }
 
     static dataParaTexto(data) {
-        return data.getDate() 
-        +  '/' + (data.getMonth() + 1)
-        + '/' + data.getFullYear();
+        return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
+        
+        // data.getDate() 
+        // +  '/' + (data.getMonth() + 1)
+        // + '/' + data.getFullYear();
     }
 
     static textoParaData(texto){
+
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto)) 
+            throw new Error('Deve estar no formato aaaa-mm-dd.');
+
         // mod 2 decrementa 1 na posicao 1 (mes)
         return new Date (...texto.split('-').map((item, indice) => item - indice % 2));
     }
